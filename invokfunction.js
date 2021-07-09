c4845335462cee89589094f84718b5770a875ae0
iamsaikatrahman <saikatrahmanoffical@gmail.com>
2021-07-09 13:04:36 +0600

    day_04

---
+++ invokfunction.js
@@ -0,0 +1,20 @@
+function userName(){
+    console.log('I am saikat rahman')
+}
+function add(){
+    var a = 10
+    var b =20
+    console.log(a+b)
+}
+function sub(){
+    var a= 50
+    var b = 30
+    console.log(a-b)
+}
+//calling function 
+userName()
+add()
+sub()
+for(var i = 0; i<=10; i++){
+    userName()
+}
